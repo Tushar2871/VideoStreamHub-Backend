3792d1599c9231654bb10e94782382f611a3639d
Tushar2871 <tgupta2871@gmail.com>
2025-04-03 20:16:22 +0530

    added multer middleware and cloudinary utility

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: "./env"
+});
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT} || 8000`);
+    });
+}).catch((error) => {
+    console.log("Error connecting to MongoDB", error);
+});
+
+
+
+
+
+
+/*
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log("Connected to MongoDB");
+    } catch (error) {
+        console.log("Error connecting to MongoDB", error);
+        process.exit(1);
+    }
+})()
+*/
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,18 @@
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error));
+};
+
+/*
+//try catch syntax for async functions
+const asyncHandler = (fn) => async (req, res, next) => {
+    try {
+        await fn(req, res, next);
+    } catch (error) {
+        res.status(error.statusCode || 500).json({
+            success: false,
+            message: error.message || "Internal Server Error",
+        });
+    }
+};
+*/
+export default asyncHandler;
