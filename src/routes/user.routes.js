bbc422d248c14f1a76111517a75da9436b036994
Tushar2871 <tgupta2871@gmail.com>
2025-04-16 02:48:15 +0530

    implement logoutUser functionality and update user routes to include logout endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,13 +5,21 @@ import { upload } from '../middlewares/multer.middleware.js';
 const router = Router();
 
 router.route("/register").post(
-    upload.fields([{
-        name:"avatar",
-        maxCount:1
-    },{
-        name:"coverImage",
-        maxCount:1
-    }]),
-    registerUser);
+    upload.fields([
+        {
+            name: "avatar",
+            maxCount: 1
+        }, 
+        {
+            name: "coverImage",
+            maxCount: 1
+        }
+    ]),
+    registerUser
+    )
+
+router.route("/login").post(loginUser)
+
+router.route("/logout").post(verifyJWT, logoutUser)
 
 export default router;
